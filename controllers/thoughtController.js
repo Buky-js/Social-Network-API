bb5670eb855d6944d2da8b5bd952b7d76b4f4bbe
Olubukola Abiona <bukyabiona@gmail.com>
2023-03-13 01:19:34 -0300

    created and tested the get routes for user and thought by id

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -2,13 +2,24 @@ const Thought = require('../models/Thought');
 
 module.exports = {
     // get all thoughts
-    getThoughts(req, res){
-Thought.find()
-.then((thoughts) => res.json(thoughts))
-.catch((err) => res.status(500).json(err));
+    getThoughts(req, res) {
+        Thought.find()
+            .then((thoughts) => res.json(thoughts))
+            .catch((err) => res.status(500).json(err));
+    },
+    // Get a single thought
+    getSingleThought(req, res) {
+        Thought.findOne({
+                _id: req.params.thoughtId
+            })
+            .select('-__v')
+            .then((thought) => {
+                !thought
+                    ?
+                    res.status(404).json({
+                        message: 'No thought with that ID!'
+                    }) : res.json(thought)
+            })
+            .catch((err) => res.status(500).json(err));
     }
 };
-
-
-
-
--- routes/api/thoughtRoute.js
+++ routes/api/thoughtRoute.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
-const { getThoughts } = require('../../controllers/thoughtController');
+const { getThoughts, getSingleThought } = require('../../controllers/thoughtController');
 
 // /api/users
 router.route('/').get(getThoughts);
 
+// /api/users/:thoughtId
+router.route('/:thoughtId').get(getSingleThought);
 
 module.exports = router;
