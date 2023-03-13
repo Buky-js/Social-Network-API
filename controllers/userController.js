b411624d0c515b2bd86616a27bb6eb00b1e98de5
Olubukola Abiona <bukyabiona@gmail.com>
2023-03-13 12:51:02 -0300

    modified the getSingleUsers route to populate thoughts and friends data

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,28 +1,51 @@
-const {User}  = require('../models');
+const {
+    User
+} = require('../models');
 
 module.exports = {
     // get all users
-    getUsers(req, res){
+    getUsers(req, res) {
         User.find()
-        .populate({ path: 'thoughts', select: '-__v' })
-        .populate({ path: 'friends', select: '-__v' })
-        // .select('-__v')
-        
-        .then((users) => res.json(users))
-        .catch((err) => {
-            console.error({ message: err });
-            res.status(500).json(err)
-        });
+            .populate({
+                path: 'thoughts',
+                select: '-__v'
+            })
+            .populate({
+                path: 'friends',
+                select: '-__v'
+            })
+            // .select('-__v')
+
+            .then((users) => res.json(users))
+            .catch((err) => {
+                console.error({
+                    message: err
+                });
+                res.status(500).json(err)
+            });
     },
-// Get a single user
-getSingleUser(req, res){
-    User.findOne({_id: req.params.userId})
-    .select('-__v')
-    .then((user) => {
-        !user 
-        ? res.status(404).json({message: 'No user with that ID!'}) : res.json(user)
-})
-.catch((err) => res.status(500).json(err));
-},
+    // Get a single user
+    getSingleUser(req, res) {
+        User.findOne({
+                _id: req.params.userId
+            })
+            .select('-__v')
+            .populate({
+                path: 'thoughts',
+                select: '-__v'
+            })
+            .populate({
+                path: 'friends',
+                select: '-__v'
+            })
+            .then((user) => {
+                !user
+                    ?
+                    res.status(404).json({
+                        message: 'No user with that ID!'
+                    }) : res.json(user)
+            })
+            .catch((err) => res.status(500).json(err));
+    },
 
 }
