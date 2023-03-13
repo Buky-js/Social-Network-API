0d3fcab4461afada2d4332066a3c473108ccfcd8
Olubukola Abiona <bukyabiona@gmail.com>
2023-03-13 00:16:32 -0300

    created and tested the api/users route

---
+++ controllers/userController.js
@@ -0,0 +1,10 @@
+const User = require('../models/User');
+
+module.exports = {
+    // get all users
+    getUsers(req, res){
+        User.find()
+        .then((users) => res.json(users))
+        .catch((err) => res.status(500).json(err));
+    }
+}
