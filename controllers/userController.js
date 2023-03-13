d693ddfa898ca8a88b3fdc819b7b789d3a596d7d
Olubukola Abiona <bukyabiona@gmail.com>
2023-03-13 13:03:07 -0300

    created and tested the post route for creating new user

--- controllers/userController.js
+++ controllers/userController.js
@@ -47,5 +47,10 @@ module.exports = {
             })
             .catch((err) => res.status(500).json(err));
     },
-
+// create a new user
+createUser(req, res) {
+    User.create(req.body)
+      .then((user) => res.json(user))
+      .catch((err) => res.status(500).json(err));
+}
 }
